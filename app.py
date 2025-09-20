58d3fb4535ac4e8979082ff829734662422b1c8a
viverna1 <viverna4141@gmail.com>
2025-09-20 12:16:17 +0400

    изменена структура

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/products')
-def products():
-    return render_template('products.html')
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # тут можно что-то делать с введёнными данными
+        return f"Ты ввёл: {username}, {password}"
+    return render_template('auth.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
